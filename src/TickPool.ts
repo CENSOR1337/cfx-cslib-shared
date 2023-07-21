ba24d61ce9225b32faf6c74a860518c2410ed741
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-21 15:11:20 +0700

    feat: Tickpool

---
+++ src/TickPool.ts
@@ -0,0 +1,35 @@
+export class Tickpool {
+	handlers = new Map<number, () => void>();
+	currentId: number;
+	tickId: number;
+
+	constructor() {
+		this.currentId = 0;
+		this.tickId = 0;
+	}
+
+	add(handler: () => void): number {
+		this.currentId++;
+		this.handlers.set(this.currentId, handler);
+		if (this.tickId === 0) {
+			this.tickId = setTick(() => {
+				this.handlers.forEach((handler) => {
+					handler();
+				});
+			});
+		}
+		return this.currentId;
+	}
+
+	remove(id: number) {
+		const handler = this.handlers.get(id);
+		if (!handler) return;
+		this.handlers.delete(id);
+		if (this.handlers.size > 0) return;
+		clearTick(this.tickId);
+	}
+
+	destroy() {
+		clearTick(this.tickId);
+	}
+}
