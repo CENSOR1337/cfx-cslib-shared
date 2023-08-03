a03fd4a0a5c7c9d92e74d7f5db400fe69213f7aa
CENSOR_1337 <viixxivvimm@gmail.com>
2023-08-03 15:32:53 +0700

    refactor:

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export { Resource } from "./Resource";
 export { Collision } from "./Collision";
 export { Callback } from "./Callback";
-export { EntityNative } from "./EntityNative";
 export { VirtualEntity } from "./VirtualEntity";
 export { WordObject } from "./WordObject";
 export { Dispatcher } from "./utils/Dispatcher";
